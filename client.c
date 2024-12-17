c2243a9c5e28a44978faab06097f3bb094d79359
Arup3201 <arupjana7365@gmail.com>
2024-12-18 01:06:28 +0530

    feat: socket server to client connect

---
+++ client.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+int main() {
+	// Create the socket for client
+	int sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock_fd < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	struct sockaddr_in name;
+	name.sin_family = AF_INET;
+	name.sin_port = htons(6666);
+	if(inet_aton("127.0.0.1", &name.sin_addr) == 0) {
+		perror("inet");
+		exit(EXIT_FAILURE);
+	}
+
+	if(connect(sock_fd, (struct sockaddr*)&name, sizeof(name)) < 0) {
+		perror("connect");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+}
--- server.c
+++ server.c
@@ -1,9 +1,48 @@
 #include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
+#include<stdlib.h>
+#include<errno.h>
 
-int main() {
-	int fd = socket(AF_INET, SOCK_STREAM, 0);
-	printf("fd=%d\n", fd);
-	return 0;
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+int main(){
+	// Create a socket
+	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+	if(sockfd<0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	// Bind network address and port with the socket
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(6666);
+	if(inet_aton("127.0.0.1", &addr.sin_addr)==0) {
+		perror("inet_aton");
+		exit(EXIT_FAILURE);
+	}
+	socklen_t length = sizeof(addr);
+	if(bind(sockfd, (struct sockaddr*)&addr, length) < 0) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	// Start listening for clients
+	if(listen(sockfd, 1) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	// Accept the client connection
+	struct sockaddr_in client;
+	socklen_t len = sizeof(client);
+	int conn_fd = accept(sockfd, (struct sockaddr*)&client, &len);
+	if(conn_fd < 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0; 
 }
