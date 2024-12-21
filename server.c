b2670faff570d64c7e3f211174eb1b015a0d71dd
Arup3201 <arupjana7365@gmail.com>
2024-12-21 12:38:45 +0530

    feat: thread based connection

--- server.c
+++ server.c
@@ -2,11 +2,14 @@
 #include<stdlib.h>
 #include<errno.h>
 #include<string.h>
+#include<threads.h>
 
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+
+int send_http_response(void*);
 
 int main(int argc, char* argv[]){
 	if(argc < 3) {
@@ -50,53 +53,42 @@ int main(int argc, char* argv[]){
 	struct sockaddr_in client;
 	socklen_t len = sizeof(client);
 
+	while(1) {
+		int conn_fd = accept(sockfd, (struct sockaddr*)&client, &len);
+		if(conn_fd < 0) {
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+		printf("Server accepted a client\n");	
+	
+
+		thrd_t p_thread;
+		thrd_create(&p_thread, send_http_response, &conn_fd);
+		thrd_detach(p_thread);
+	}
+	return 0; 
+}
+
+int send_http_response(void* fd) {
+	int *conn_fd = (int*)fd;
 	size_t buffer_len = 2048;
 	char http_request[buffer_len];
 	char http_response[buffer_len];
 
-	int conn_fd = accept(sockfd, (struct sockaddr*)&client, &len);
-	if(conn_fd < 0) {
-		perror("accept");
-		exit(EXIT_FAILURE);
+	// Reset the address with null
+	memset(http_response, '\0', buffer_len);
+	memset(http_request, '\0', buffer_len);
+
+	ssize_t nbytes = recv(*conn_fd, http_request, buffer_len, 0);
+	// Send response to browser
+	if(nbytes > 0) {
+		strcpy(http_response, "HTTP/1.1 200 OK\r\nConnection: close\r\n");
+		strcat(http_response, "\nHello");
+		printf("%s\n\n", http_response);
+		send(*conn_fd, http_response, buffer_len, 0);
+	} else {
+		printf("Client disconnected!\n");
 	}
-	printf("Server accepted a client\n");
-	
-	fd_set read_fd;
-	FD_ZERO(&read_fd);
-	FD_SET(conn_fd, &read_fd);
-	
-	struct timeval timeout;
-	timeout.tv_sec = 2;
-	timeout.tv_usec = 0;
 
-	while(1) {
-		// Check everytime the file descriptor
-		if(select(conn_fd+1, &read_fd, NULL, NULL, &timeout) < 0) {
-			perror("select");
-			exit(EXIT_FAILURE);
-		}
-
-		// Check whether the file descriptor of the accept is set
-		if(FD_ISSET(conn_fd, &read_fd)) {
-			printf("Client requesting...\n\n");
-			// Reset the address with null
-			memset(http_response, '\0', buffer_len);
-			memset(http_request, '\0', buffer_len);
-
-			ssize_t nbytes = recv(conn_fd, http_request, buffer_len, 0);
-
-			// Send response to browser
-			if(nbytes > 0) {
-				strcpy(http_response, "HTTP/1.1 200 OK\nConnection: close\n");
-				strcat(http_response, "\nHello");
-				printf("%s\n\n", http_response);
-				send(conn_fd, http_response, buffer_len, 0);
-			} else {
-				printf("Client disconnected!\n");
-				break;
-			}
-		}
-	}
-	
-	return 0; 
+	return 1;
 }
