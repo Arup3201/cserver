e8fda56c85944f15f171698ad3006786e9f3e7c0
Arup Jana <arup@Ubuntu.myguest.virtualbox.org>
2025-02-20 20:58:04 +0530

    get server and response loop added in connection

--- connection/connection.c
+++ connection/connection.c
@@ -1,2 +1,54 @@
 #include "connection.h"
 
+#ifndef _STDIO_H
+#include<stdio.h>
+#endif
+
+#ifndef	_STDLIB_H
+#include<stdlib.h>
+#endif
+
+#ifndef _ARPA_INET_H
+#include<arpa/inet.h>
+#endif
+
+host_t conn_get_server(char* address, int port) {
+	host_t server = (host_t)malloc(sizeof(struct host));
+
+	server->fd = socket(PF_INET, SOCK_STREAM, 0);
+
+	server->addr.sin_family = AF_INET;
+	server->addr.sin_port = htons(port);
+
+	if(inet_aton(address, &server->addr.sin_addr) == 0) {
+		perror("inet-aton");
+		exit(EXIT_FAILURE);
+	}
+
+	if(bind(server->fd, (struct sockaddr*)&server->addr, server->length) < 0) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	return server;
+}
+
+void conn_response_loop(host_t server, response_handler_t handler) {
+	if(listen(server->fd, 1) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	host_t client = (host_t)malloc(sizeof(struct host));
+
+	while(1) {
+		client->fd = accept(server->fd, (struct sockaddr*)&client->addr, &client->length);
+		if(client->fd < 0) {
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+
+		printf("Server accepted a client\n");
+		(*handler)(&client->fd);
+	}
+}
--- connection/connection.h
+++ connection/connection.h
@@ -1,8 +1,23 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
+#ifndef	_SYS_SOCKET_H
+#include<sys/socket.h>
+#endif
 
+#ifndef	_NETINET_IN_H
+#include<netinet/in.h>
+#endif
+
+typedef struct host{
+	int fd;
+	struct sockaddr_in addr;
+	socklen_t length;
+}*host_t;
+
+typedef void (*response_handler_t)(int*) ;
+
+host_t conn_get_server(char*, int);
+void connn_response_loop(host_t, response_handler_t);
 
 #endif // !CONNECTION_H
-
-
